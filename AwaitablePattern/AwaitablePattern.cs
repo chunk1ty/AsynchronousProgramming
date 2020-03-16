b97238350ef312d542ee0b6ab4452c990e6eada1
chunk1ty <andriyan.krastev@gmail.com>
2020-03-16 06:24:52 +0200

    added Awaitable Pattern

---
+++ AwaitablePattern/AwaitablePattern.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace AwaitablePattern
+{
+    public class MyAwaitableClass
+    {
+        public MyAwaiter GetAwaiter()
+        {
+            return new MyAwaiter();
+        }
+    }
+
+    public class MyAwaiter : INotifyCompletion
+    {
+        public void GetResult()
+        {
+        }
+
+        public bool IsCompleted
+        {
+            get { return false; }
+        }
+
+        //From INotifyCompletion
+        public void OnCompleted(Action continuation)
+        {
+        }
+    }
+}
---
+++ AwaitablePattern/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AwaitablePattern
+{
+    class Program
+    {
+        static async void Main(string[] args)
+        {
+            MyAwaitableClass awaitableObject = new MyAwaitableClass();
+
+            await awaitableObject;
+        }
+    }
+}
