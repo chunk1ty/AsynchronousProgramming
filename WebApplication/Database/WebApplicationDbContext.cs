e7f49466717b42ff8dffbcc30a1e0bb656f2f213
chunk1ty <andriyan.krastev@gmail.com>
2020-03-19 00:09:56 +0200

    added WebApplicationDbContext

---
+++ WebApplication/Database/WebApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Database
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public class WebApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+           // optionsBuilder.UseSqlServer(@"Server=.;Database=WebApplicationDbContext;Trusted_Connection=True;");
+
+            optionsBuilder.UseSqlServer(@"Server=.;Database=WebApplicationDbContext;User Id=sa;password=<YourStrong@Passw0rd>;Trusted_Connection=False;MultipleActiveResultSets=true;");
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
