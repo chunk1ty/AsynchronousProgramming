b7ccf9cb4f0733d999531e9a84c4e5470f922944
chunk1ty <andriyan.krastev@gmail.com>
2020-03-28 16:00:48 +0200

    added State Machine decompiled example

---
+++ StateMachine/AsyncMethod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace StateMachine
+{
+    class AsyncMethod
+    {
+        static void Main(string[] args)
+        {
+            PrintAndWait(TimeSpan.FromSeconds(1)).Wait();
+
+            //AsyncMethodDecompiled.MainAsync();
+        }
+
+        static async Task PrintAndWait(TimeSpan delay)
+        {
+            Console.WriteLine("Before first delay");
+            await Task.Delay(delay);
+            Console.WriteLine("Between delays");
+            await Task.Delay(delay);
+            Console.WriteLine("After second delay");
+        }
+    }
+}
---
+++ StateMachine/AsyncMethodDecompiled.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace StateMachine
+{
+    public class AsyncMethodDecompiled
+    {
+        public static void MainAsync()
+        {
+            PrintAndWait(TimeSpan.FromSeconds(1)).Wait();
+        }
+
+        [AsyncStateMachine(typeof(PrintAndWaitStateMachine))]
+        [DebuggerStepThrough]
+        private static Task PrintAndWait(TimeSpan delay)
+        {
+            var machine = new PrintAndWaitStateMachine
+            {
+                delay = delay,
+                builder = AsyncTaskMethodBuilder.Create(),
+                state = -1
+            };
+            machine.builder.Start(ref machine);
+            return machine.builder.Task;
+        }
+
+        [CompilerGenerated]
+        private struct PrintAndWaitStateMachine : IAsyncStateMachine
+        {
+            public int state;
+            public AsyncTaskMethodBuilder builder;
+            private TaskAwaiter awaiter;
+            public TimeSpan delay;
+
+            void IAsyncStateMachine.MoveNext()
+            {
+                int num = state;
+                try
+                {
+                    TaskAwaiter awaiter1;
+                    switch (num)
+                    {
+                        default:
+                            goto MethodStart;
+                        case 0:
+                            goto FirstAwaitContinuation;
+                        case 1:
+                            goto SecondAwaitContinuation;
+                    }
+                MethodStart:
+                    Console.WriteLine("Before first delay");
+                    awaiter1 = Task.Delay(delay).GetAwaiter();
+                    if (awaiter1.IsCompleted)
+                    {
+                        goto GetFirstAwaitResult;
+                    }
+                    state = num = 0;
+                    awaiter = awaiter1;
+                    builder.AwaitUnsafeOnCompleted(ref awaiter1, ref this);
+                    return;
+                FirstAwaitContinuation:
+                    awaiter1 = awaiter;
+                    awaiter = default(TaskAwaiter);
+                    state = num = -1;
+                GetFirstAwaitResult:
+                    awaiter1.GetResult();
+                    Console.WriteLine("Between delays");
+                    TaskAwaiter awaiter2 = Task.Delay(delay).GetAwaiter();
+                    if (awaiter2.IsCompleted)
+                    {
+                        goto GetSecondAwaitResult;
+                    }
+                    state = num = 1;
+                    awaiter = awaiter2;
+                    builder.AwaitUnsafeOnCompleted(ref awaiter2, ref this);
+                    return;
+                SecondAwaitContinuation:
+                    awaiter2 = awaiter;
+                    awaiter = default(TaskAwaiter);
+                    state = num = -1;
+                GetSecondAwaitResult:
+                    awaiter2.GetResult();
+                    Console.WriteLine("After second delay");
+                }
+                catch (Exception exception)
+                {
+                    state = -2;
+                    builder.SetException(exception);
+                    return;
+                }
+                state = -2;
+                builder.SetResult();
+            }
+
+            [DebuggerHidden]
+            void IAsyncStateMachine.SetStateMachine(IAsyncStateMachine stateMachine)
+            {
+                this.builder.SetStateMachine(stateMachine);
+            }
+        }
+    }
+}
