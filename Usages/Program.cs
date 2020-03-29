4324d5d4526f8d4dc84306da4b2e6bb7e4bc2775
chunk1ty <andriyan.krastev@gmail.com>
2020-03-29 02:26:42 +0200

    added reporting progress example

--- Usages/Program.cs
+++ Usages/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Usages
 {
@@ -6,7 +7,11 @@ namespace Usages
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+
+            ReportingProgress.CallMyMethodAsync().GetAwaiter().GetResult();
         }
+
+       
     }
 }
---
+++ Usages/ReportingProgress.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Usages
+{
+    public class ReportingProgress
+    {
+        // Problem
+        // You need to respond to progress while an asynchronous operation is executing.
+
+        // Solution
+        // Use the provided IProgress<T> and Progress<T> types.
+        static async Task MyMethodAsync(IProgress<int> progress = null)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                await Task.Delay(100);
+
+                if (progress != null)
+                {
+                    progress.Report(i);
+                }
+            }
+        }
+
+        static async Task CallMyMethodAsync()
+        {
+            //var progress = new Progress<int>();
+
+            //progress.ProgressChanged += (sender, args) =>
+            //{
+            //    Console.WriteLine(args + "%");
+            //};
+
+            var progress = new Progress<int>(percent =>
+            {
+                Console.WriteLine(percent + "%");
+            });
+
+            await MyMethodAsync(progress);
+        }
+    }
+}
